27c03f25ab26c5fb27f92bf6fdd50ba239baa452
Patrick Allen <allenpatrick86@gmail.com>
2016-06-27 22:43:12 -0700

    add logic for presenting open jobs and open routes along with price

--- app/controllers/the_grid_controller.rb
+++ app/controllers/the_grid_controller.rb
@@ -4,6 +4,8 @@ class TheGridController < ApplicationController
   # GET /thegrid.json
   def index
     @users = User.all
+    @independent_jobs = Job.where(route_id: nil)
+    @open_routes = Route.where(employee_id: nil)
   end
 
 end
