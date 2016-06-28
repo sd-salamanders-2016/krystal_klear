a1d16de5cb9aab5088bb6fede1abc974432323a8
Gabriel Bonner <gabrielbonner@gmail.com>
2016-06-28 11:12:26 -0700

    Add model work_order tests

--- spec/models/work_order_spec.rb
+++ spec/models/work_order_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe WorkOrder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe WorkOrder do
+  let (:work_order) {WorkOrder.new(route_id: 1,
+                                   job_id: 1,
+                                   final_price: 30.00,
+                                   notes: 'Finish before 9am',
+                                   week: 26)}
+
+  it 'has a route_id' do
+    expect(work_order.route_id).to be_a Integer
+  end
+
+  it 'has a job_id' do
+    expect(work_order.job_id).to be_a Integer
+  end
+
+  it 'has a final_price' do
+    expect(work_order.final_price).to be_a Float
+  end
+
+  it 'can have notes' do
+    expect(work_order.notes).to be_a String
+  end
+
+  it 'has an assiged week' do
+    expect(work_order.week).to be_a Integer
+  end
+
+  it 'has a default value of incomplete' do
+    expect(work_order.complete).to eq 'incomplete'
+  end
+
 end
