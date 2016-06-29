aa4632485384e4c7578f913e222853e1e55a9d98
Gabriel Bonner <gabrielbonner@gmail.com>
2016-06-29 14:48:51 -0700

    most recent updates

--- app/controllers/the_grid_controller.rb
+++ app/controllers/the_grid_controller.rb
@@ -6,6 +6,7 @@ class TheGridController < ApplicationController
     @users = User.all
     @independent_jobs = Job.where(route_id: nil)
     @open_routes = Route.where(employee_id: nil)
+    @open_routes = @open_routes.sort { |prev, curr| curr.sum_jobs <=> prev.sum_jobs }
   end
 
 end
