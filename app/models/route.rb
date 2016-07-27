478b2a299ed49afa318900823dddc7853ff7507e
Patrick Allen <allenpatrick86@gmail.com>
2016-07-26 22:53:55 -0700

    clean up comments

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,5 @@
 class Route < ActiveRecord::Base
   has_many :jobs
-  # has_many :employee_routes
-  # has_many :job_employees, through: :employee_routes, source: :user
   has_many :work_orders
   has_many :current_week_work_orders, -> {where(week: Time.now.strftime('%W').to_i)}, class_name: 'WorkOrder'
   belongs_to :employee, class_name: "User"
